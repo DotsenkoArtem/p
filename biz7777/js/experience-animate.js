4a6de7c50345938c82374e6c30ab56563bea6642
DotsenkoArtem <artemdoc1@rambler.ru>
2023-04-09 14:04:33 +0500

    BLOCK3: убраны console.log~

--- biz7777/js/experience-animate.js
+++ biz7777/js/experience-animate.js
@@ -7,30 +7,23 @@ function expBlockAnimate(elemClass) {
 
   if (block) {
     this.setTimeout(() => {
-
       // ТЕКУЩИЕ КООРДИНАТЫ ЭЛЕМЕНТОВ
       // БЛОК
       // Текущий отступ блока по оси Y от верхней границы окна
       let currentBlockTop = block.getBoundingClientRect().top;
       let currentBlockBottom = block.getBoundingClientRect().bottom;
 
-      let expAnimStartPoint
-      let expAnimStopPoint
-
+      let expAnimStartPoint;
+      let expAnimStopPoint;
 
       const expBoxIconItem = block.querySelector(".block3__box-icon");
       const expBoxIconItems = expBoxIconItem.querySelectorAll(".exp-anim");
 
-      let expTextAnimItems = Array.from(expBoxIconItems)
-      expTextAnimItems.push(block.querySelector(".selection-text"), block.querySelector(".selection-title"))
-
-    
-
-
-
-
-
-
+      let expTextAnimItems = Array.from(expBoxIconItems);
+      expTextAnimItems.push(
+        block.querySelector(".selection-text"),
+        block.querySelector(".selection-title")
+      );
 
       const expScene = block.querySelector(".exp-scene");
       const expSceneItems = expScene.querySelectorAll(".exp-anim");
@@ -60,15 +53,18 @@ function expBlockAnimate(elemClass) {
         currentBlockTop = block.getBoundingClientRect().top;
         currentBlockBottom = block.getBoundingClientRect().bottom;
         getScrollDirection();
-        console.log('scrollDirection: ', scrollDirection);
-
 
         if (unLockedDocumentWidth > 1000) {
-          expAnimStartPoint = window.innerHeight - block.getBoundingClientRect().height;
+          expAnimStartPoint =
+            window.innerHeight - block.getBoundingClientRect().height;
           expAnimStopPoint = block.getBoundingClientRect().height + 100;
           if (
-            (currentBlockTop <= expAnimStartPoint && currentBlockBottom > expAnimStartPoint && scrollDirection < 0) ||
-            (currentBlockBottom >= expAnimStopPoint && currentBlockTop < expAnimStopPoint && scrollDirection > 0)
+            (currentBlockTop <= expAnimStartPoint &&
+              currentBlockBottom > expAnimStartPoint &&
+              scrollDirection < 0) ||
+            (currentBlockBottom >= expAnimStopPoint &&
+              currentBlockTop < expAnimStopPoint &&
+              scrollDirection > 0)
           ) {
             expAnimItems.forEach((item) => {
               item.style.visibility = ``;
@@ -99,40 +95,25 @@ function expBlockAnimate(elemClass) {
         }
 
         if (unLockedDocumentWidth <= 1000) {
-          // expAnimStartPoint = window.innerHeight * 0.8;
-          // expAnimStopPoint = window.innerHeight * 0.2;
           let currAnimItems = 0;
-          
 
           expTextAnimItems.forEach((item) => {
-
-
-                          // if (
-            //   (currentBlockTop <= expAnimStartPoint && currentBlockBottom > expAnimStartPoint && scrollDirection < 0) ||
-            //   (currentBlockBottom >= expAnimStopPoint && currentBlockTop < expAnimStopPoint && scrollDirection > 0)
-            // ) {
-
-
-
-
             if (
-              (item.getBoundingClientRect().top <= window.innerHeight - item.getBoundingClientRect().height &&
-              item.getBoundingClientRect().bottom > window.innerHeight - item.getBoundingClientRect().height &&
+              (item.getBoundingClientRect().top <=
+                window.innerHeight - item.getBoundingClientRect().height &&
+                item.getBoundingClientRect().bottom >
+                  window.innerHeight - item.getBoundingClientRect().height &&
                 scrollDirection < 0) ||
-              (item.getBoundingClientRect().bottom >= item.getBoundingClientRect().height + 100 &&
-                item.getBoundingClientRect().top < item.getBoundingClientRect().height + 100 &&
+              (item.getBoundingClientRect().bottom >=
+                item.getBoundingClientRect().height + 100 &&
+                item.getBoundingClientRect().top <
+                  item.getBoundingClientRect().height + 100 &&
                 scrollDirection > 0)
             ) {
               item.style.visibility = ``;
               item.style.animationName = ``;
 
               currAnimItems++;
-
-              // if (item.classList.contains("exp-hart")) {
-              //   setTimeout(() => {
-              //     item.classList.add("move-hart");
-              //   }, 1000);
-              // }
 
               if (item.classList.contains("block3__container-icon")) {
                 setTimeout(() => {
@@ -141,22 +122,23 @@ function expBlockAnimate(elemClass) {
               }
             }
           });
-          
 
           expSceneItems.forEach((item) => {
-
             if (
-              (expScene.getBoundingClientRect().top <= window.innerHeight - expScene.getBoundingClientRect().height &&
-              expScene.getBoundingClientRect().bottom > window.innerHeight - expScene.getBoundingClientRect().height &&
+              (expScene.getBoundingClientRect().top <=
+                window.innerHeight - expScene.getBoundingClientRect().height &&
+                expScene.getBoundingClientRect().bottom >
+                  window.innerHeight -
+                    expScene.getBoundingClientRect().height &&
                 scrollDirection < 0) ||
-              (expScene.getBoundingClientRect().bottom >= expScene.getBoundingClientRect().height + 100 &&
-                expScene.getBoundingClientRect().top < expScene.getBoundingClientRect().height + 100 &&
+              (expScene.getBoundingClientRect().bottom >=
+                expScene.getBoundingClientRect().height + 100 &&
+                expScene.getBoundingClientRect().top <
+                  expScene.getBoundingClientRect().height + 100 &&
                 scrollDirection > 0)
             ) {
               item.style.visibility = ``;
               item.style.animationName = ``;
-
-              
 
               if (item.classList.contains("exp-hart")) {
                 setTimeout(() => {
