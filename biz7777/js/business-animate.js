e6a91d7600987a417ac3d43d570153ea12a6b411
DotsenkoArtem <artemdoc1@rambler.ru>
2023-04-17 23:39:21 +0500

    BLOCK5: branch6 - вроде бы слайдер без проскальзываний

--- biz7777/js/business-animate.js
+++ biz7777/js/business-animate.js
@@ -55,78 +55,213 @@ function blockHorizontalScroll(
   transition
 ) {
   if (unLockedDocumentWidth > 1000) {
+    const block = document.querySelector(`.block6`);
+    let blockTop = block.getBoundingClientRect().top;
+    let blockBottom = block.getBoundingClientRect().bottom;
+
+    let content = document.querySelector(".content");
     let section = document.querySelector(`.${sectionClassName}`);
+    let sectionTop = section.getBoundingClientRect().top;
     let scrollingElem = section.querySelector(`.${scrollingElemClassName}`);
     let elTranslateX = 0;
     let startPoint = window.innerHeight * 0.75;
     let stopPoint =
       scrollingElem.getBoundingClientRect().height + window.innerHeight * 0.55;
 
-    // let scrollDirection = 0;
-    // let startY = window.pageYOffset;
-    // let currentY = 0;
-    // function getScrollDirection() {
-    //   currentY = window.pageYOffset;
-    //   scrollDirection = startY - currentY;
-    //   startY = currentY;
-    // }
 
-    window.addEventListener("scroll", lockBySectionPosition);
 
-    function lockBySectionPosition() {
-      // getScrollDirection();
-      let scrollingElemTop = scrollingElem.getBoundingClientRect().top;
-      let scrollingElemBottom = scrollingElem.getBoundingClientRect().bottom;
-      let scrollingElemWIdth = scrollingElem.scrollWidth;
+
+
+    let isInWindow = false
+    let isLocked = false
+
+    // window.addEventListener("scroll", lockBySectionPosition);
+
+    window.addEventListener("scroll", () => {
+      blockTop = block.getBoundingClientRect().top;
+      blockBottom = block.getBoundingClientRect().bottom;
+      sectionTop = section.getBoundingClientRect().top;
+
+      if(blockTop > document.documentElement.clientHeight * .6 && scrollDirection < 0 || blockBottom < document.documentElement.clientHeight * .4 && scrollDirection > 0) {
+        isInWindow = false
+      }
+      // if(sectionTop > document.documentElement.clientHeight / 2 && scrollDirection > 0
+      // ||
+      // sectionTop < document.documentElement.clientHeight / 2 && scrollDirection < 0) {
+      //   isInWindow = false
+      // }
+      console.log('scrollDirection: ', scrollDirection);
 
       if (
-        (scrollingElemTop < startPoint &&
-          scrollingElemBottom > startPoint &&
-          scrollDirection < 0 &&
-          elTranslateX !== -(scrollingElemWIdth - scrollingElem.offsetWidth)) ||
-        (scrollingElemBottom > stopPoint &&
-          scrollingElemTop < stopPoint &&
-          scrollDirection > 0 &&
-          elTranslateX !== 0)
+        (blockTop <= document.documentElement.clientHeight && blockTop >= 0 && !isInWindow) ||
+        (blockBottom >= 0 &&
+          blockBottom <= document.documentElement.clientHeight  && !isInWindow)
       ) {
+
+  
         lockPage(unLockedDocumentWidth, header);
 
-        let content = document.querySelector(".content");
-        content.addEventListener("wheel", scrollBlock);
-        function scrollBlock(e) {
-          e.preventDefault();
-          scrollingElem.style.transition = `${transition}ms`;
-          elTranslateX -= e.deltaY * 0.75;
-          if (elTranslateX >= 0) {
-            elTranslateX = 0;
-            scrollingElem.style.transform = `translateX(${elTranslateX}px)`;
+        setTimeout(()=>{
+          scrollToCenter()
+        },0)
+        
 
-            setTimeout(() => {
-              unLockPage(header);
-              content.removeEventListener("wheel", scrollBlock);
-              scrollingElem.style.transition = ``;
-              window.addEventListener("scroll", lockBySectionPosition);
-            }, transition);
-          }
-
-          if (
-            Math.abs(elTranslateX) >=
-            scrollingElemWIdth - scrollingElem.offsetWidth
-          ) {
-            elTranslateX = -(scrollingElemWIdth - scrollingElem.offsetWidth);
-            scrollingElem.style.transform = `translateX(${elTranslateX}px)`;
-
-            setTimeout(() => {
-              unLockPage(header);
-              content.removeEventListener("wheel", scrollBlock);
-              scrollingElem.style.transition = ``;
-              window.addEventListener("scroll", lockBySectionPosition);
-            }, transition);
-          } else {
-            scrollingElem.style.transform = `translateX(${elTranslateX}px)`;
-          }
-        }
       }
+    });
+
+    // content.addEventListener("wheel", scrollToCenter);
+    let scrolledToCenter = false;
+    let currentScroll = undefined;
+    let targetScroll = undefined;
+    function scrollToCenter() {
+      if(!isInWindow) { 
+        // let sectionTop = section.getBoundingClientRect().top;
+        currentScroll = window.scrollY;
+        targetScroll = sectionTop - document.documentElement.clientHeight / 2;
+
+        
+        animateScrollToCenter({
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            window.scrollTo(
+              0,
+              currentScroll + targetScroll * progress
+            );
+          },
+          duration: Math.abs(targetScroll * .6),
+          // duration: 750,
+        });
+        isInWindow = true
+        setTimeout(()=>{
+          isLocked = true
+          // unLockPage(header)
+        }, Math.abs(targetScroll * .6))
+        // }, 2000)
+      }
+
+
+
+
+    }
+
+    function animateScrollToCenter({ timing, draw, duration }) {
+      let start = performance.now();
+
+      requestAnimationFrame(function animateScrollToCenter(time) {
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+        // Линейная функция
+        let progress = timing(timeFraction);
+        // let progress =
+        //   timing(timeFraction) < 0.5
+        //     ? 4 *
+        //       timing(timeFraction) *
+        //       timing(timeFraction) *
+        //       timing(timeFraction)
+        //     : 1 - Math.pow(-2 * timing(timeFraction) + 2, 3) / 2;
+        draw(progress); // отрисовать её
+        if (timeFraction < 1) {
+          requestAnimationFrame(animateScrollToCenter);
+        }
+      });
+    }
+
+    content.addEventListener("wheel", lockBySectionPosition);
+
+    function lockBySectionPosition(e) {
+      // console.log('scrollDirection: ', scrollDirection);
+
+      if(isInWindow && isLocked) {
+
+
+        let scrollingElemTop = scrollingElem.getBoundingClientRect().top;
+        let scrollingElemBottom = scrollingElem.getBoundingClientRect().bottom;
+        let scrollingElemWIdth = scrollingElem.scrollWidth;
+
+        // if (
+          // // Скроллим вниз
+          // (
+          //   scrollDirection < 0 
+          //   && elTranslateX <= 0 && elTranslateX > (scrollingElem.offsetWidth - scrollingElemWIdth)
+          //   )
+          //   ||
+          //   // Скроллим вверх
+          // (
+          //   scrollDirection > 0 
+          //   && elTranslateX >= (scrollingElem.offsetWidth - scrollingElemWIdth) && elTranslateX < 0
+          //   )
+        // ) {
+          
+          
+
+
+          scrollBlock()
+          // content.addEventListener("wheel", scrollBlock);
+          function scrollBlock() {
+            // e.preventDefault();
+            scrollingElem.style.transition = `${transition}ms`;
+            elTranslateX -= e.deltaY * 0.75;
+            console.log('elTranslateX: ', elTranslateX);
+            
+            if (elTranslateX >= 0 && e.deltaY < 0) {
+              elTranslateX = 0;
+              scrollingElem.style.transform = `translateX(${0}px)`;
+              isLocked = false
+              // unLockPage(header);
+              console.log('elTranslateX: ', elTranslateX);
+              console.log('e.defaultPrevented: ', e.defaultPrevented);
+             
+
+              setTimeout(() => {
+                unLockPage(header);
+                // content.removeEventListener("wheel", scrollBlock);
+                scrollingElem.style.transition = ``;
+              //   window.addEventListener("scroll", lockBySectionPosition);
+              }, transition);
+            }
+
+            else if (
+              Math.abs(elTranslateX) >=  Math.abs(scrollingElem.offsetWidth - scrollingElemWIdth) && e.deltaY > 0
+            ) {
+              elTranslateX = scrollingElem.offsetWidth - scrollingElemWIdth;
+              scrollingElem.style.transform = `translateX(${elTranslateX}px)`;
+              isLocked = false
+              console.log('В КОНЦЕ МЫ: ');
+              // unLockPage(header);
+              setTimeout(() => {
+                unLockPage(header);
+              //   content.removeEventListener("wheel", scrollBlock);
+                scrollingElem.style.transition = ``;
+              //   window.addEventListener("scroll", lockBySectionPosition);
+              }, transition);
+            } else {
+              // isLocked = true
+              lockPage(unLockedDocumentWidth, header);
+              e.preventDefault();
+              scrollingElem.style.transform = `translateX(${elTranslateX}px)`;
+              console.log('elTranslateX: ', elTranslateX);
+            }
+
+            
+
+          }
+        // }
+        // if(elTranslateX)
+
+
+
+
+
+
+
+      }
+
+
+
+
+
     }
   }
 }
